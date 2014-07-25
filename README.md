d9743cd0fb67fcf06ff538de3131fd1f3b222fcb
Eric D. Schabell <eric@schabell.org>
2014-07-25 12:21:37 +0200

    Added initial import.

---
+++ README.md
@@ -0,0 +1,36 @@
+JBoss BPM Suite Helpdesk Demo 
+=============================
+
+Demonstration project for helpdesk assignment of initial callers to available helpdesk personel on the basis of qualifications to
+support language and timezone of caller.
+
+
+Quickstart
+----------
+
+1. Clone project.
+
+2. Add products to installs directory.
+
+3. Run 'init.sh' or 'init.bat' file.
+
+4. Login to http://localhost:8080/business-central  (u:erics / p:bpmsuite1!)
+
+5. Enjoy demo!
+
+
+Supporting Articles
+-------------------
+
+None yet...
+
+
+Released versions
+-----------------
+
+See the tagged releases for the following versions of the product:
+
+- v0.1 - JBoss BPM SUite 6.0.2 installer with demo installed.
+
+
+![BPM Suite](https://github.com/eschabell/bpms-helpdesk-demo/blob/master/docs/demo-images/bpmsuite.png?raw=true)
---
+++ init.sh
@@ -0,0 +1,88 @@
+#!/bin/sh 
+DEMO="JBoss BPM Suite Helpdesk Demo"
+AUTHORS="Eric D. Schabell"
+PROJECT="git@github.com:eschabell/bpms-helpdesk-demo.git"
+PRODUCT="JBoss BPM Suite"
+JBOSS_HOME=./target/jboss-eap-6.1
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+SUPPORT_DIR=./support
+PRJ_DIR=./projects
+BPMS=jboss-bpms-installer-6.0.2.GA-redhat-5.jar
+VERSION=6.0.2
+
+# wipe screen.
+clear 
+
+echo
+echo "#################################################################"
+echo "##                                                             ##"   
+echo "##  Setting up the ${DEMO}               ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##     ####  ####   #   #      ### #   # ##### ##### #####     ##"
+echo "##     #   # #   # # # # #    #    #   #   #     #   #         ##"
+echo "##     ####  ####  #  #  #     ##  #   #   #     #   ###       ##"
+echo "##     #   # #     #     #       # #   #   #     #   #         ##"
+echo "##     ####  #     #     #    ###  ##### #####   #   #####     ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##  brought to you by,                                         ##"   
+echo "##             ${AUTHORS}                                ##"
+echo "##                                                             ##"   
+echo "##  ${PROJECT}            ##"
+echo "##                                                             ##"   
+echo "#################################################################"
+echo
+
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
+		echo Product sources are present...
+		echo
+else
+		echo Need to download $BPMS package from the Customer Portal 
+		echo and place it in the $SRC_DIR directory to proceed...
+		echo
+		exit
+fi
+
+# Move the old JBoss instance, if it exists, to the OLD position.
+if [ -x $JBOSS_HOME ]; then
+		echo "  - existing JBoss product install detected..."
+		echo
+		echo "  - moving existing JBoss product install aside..."
+		echo
+		rm -rf $JBOSS_HOME.OLD
+		mv $JBOSS_HOME $JBOSS_HOME.OLD
+fi
+
+# Run installer.
+echo Product installer running now...
+echo
+java -jar $SRC_DIR/$BPMS $SUPPORT_DIR/installation-bpms -variablefile $SUPPORT_DIR/installation-bpms.variables
+
+echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo
+cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+
+echo "  - setting up standalone.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone.xml $SERVER_CONF
+
+# Add execute permissions to the standalone.sh script.
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo "You can now start the $PRODUCT with $SERVER_BIN/standalone.sh"
+echo
+echo "Login to http://localhost:8080/business-central   (u:erics / p:bpmsuite1!)"
+echo
+
+echo "$PRODUCT $VERSION $DEMO Setup Complete."
+echo
+
